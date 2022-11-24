8d26dcaf2a1bafda018724cfe0a5e05ac981d68d
Tmeng <tonymengt@gmail.com>
2022-11-23 21:09:49 -0500

    resovled backend authmiddleware issue

--- server/server.js
+++ server/server.js
@@ -54,7 +54,7 @@ const db = require("./config/connection");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
 
 const server = new ApolloServer({
